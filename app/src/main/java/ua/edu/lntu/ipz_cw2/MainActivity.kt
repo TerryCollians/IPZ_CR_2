8f55ed0745898935afdab0b6aa829dbcfa0dd7ee
Владислав Павлик <144818450+TerryCollians@users.noreply.github.com>
2024-03-01 18:16:53 +0100

    added column interface

--- app/src/main/java/ua/edu/lntu/ipz_cw2/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cw2/MainActivity.kt
@@ -3,7 +3,9 @@ package ua.edu.lntu.ipz_cw2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -11,8 +13,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ua.edu.lntu.ipz_cw2.ui.theme.IPZ_CR_2Theme
 
 class MainActivity : ComponentActivity() {
@@ -39,6 +43,18 @@ fun SliderWithButton(){
 
     val currentIndex by remember { mutableStateOf(0) }
     // змінна для початкового індексу
+
+
+    // Графічне відображення інтерфейсу
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(18.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+
+    }
 }
 
 @Preview(showBackground = true)
